1fdcbe9e1911418a486fd110f85950c9a6144106
devbrayan2007 <devbrayan2007@gmail.com>
2024-07-18 19:03:54 -0300

    aula04

--- Estrutura Básica/aula04.py
+++ Estrutura Básica/aula04.py
@@ -1,2 +1,11 @@
 # ENTRADA DE DADOS
 
+produto = str(input("Digite o nome do produto: "))
+print(produto)
+
+codigo = int(input("Informe o código: "))
+nome = str(input("Digite seu nome: "))
+salario = str(input("Informe seu salário: R$"))
+situação = bool(input("Informe sua situação atual: "))
+
+print(f"Código: {codigo}\n Nome: {nome}\n Salário: R${salario}\n Situação: {situação}")
