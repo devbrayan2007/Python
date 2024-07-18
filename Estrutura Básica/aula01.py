2f92f4c53cba304e48155d888e85238f830b7517
devbrayan2007 <devbrayan2007@gmail.com>
2024-07-18 18:47:01 -0300

    aula01

--- Estrutura Básica/aula01.py
+++ Estrutura Básica/aula01.py
@@ -9,3 +9,21 @@ x = 10
 cidade = 'Rio de Janeiro'
 
 # TIPOS DE DADOS
+
+codigo = 14
+nome = "Brayan Campos"
+salario = 4.500
+situação = True
+
+print(type(codigo)) # int
+print(type(nome)) # str
+print(type(salario)) # float
+print(type(situação)) # bool
+
+# Concatenando variáveis
+
+print("Código: ", codigo, " Nome: ", nome, " Salário: R$",salario, " Situação: ", situação)
+
+# Usando o print formatado
+print(f"Código: {codigo} | Nome: {nome} | Salário: R${salario} | Situação: {situação}")
+
